f85003bb9a398cf17d16afeab8c090b3458d5308
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-09-07 12:38:20 +0530

    Routes

--- Backend/Routes/Product.Routes.js
+++ Backend/Routes/Product.Routes.js
@@ -38,7 +38,8 @@ router.post("/create", isAuthenticated, upload.single("image"), async (req, res)
 router.get("/:id", async (req, res) => {
     try {
         let productName = req.params.id
-        productName = productName.replaceAll('-', ' ').replaceAll('&', ' ')
+        productName = productName.replaceAll('-', ' ').replaceAll('&', '/')
+        console.log(productName)
         const product = await Product.findOne({name: productName});
         if (!product) {
             return res.status(404).json({ success: false, message: "Product not found" });
